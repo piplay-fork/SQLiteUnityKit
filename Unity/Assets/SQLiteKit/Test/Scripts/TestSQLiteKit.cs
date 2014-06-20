a68c0bde5a5a5bd70ec65ebc63111f4e7827f3c9
paynechu <314176+paynechu@users.noreply.github.com>
2014-06-20 19:27:02 +0800

    - exposed database file path and exception if have

--- Unity/Assets/SQLiteKit/Test/Scripts/TestSQLiteKit.cs
+++ Unity/Assets/SQLiteKit/Test/Scripts/TestSQLiteKit.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Collections;
 using LiteKit.Data;
 using LiteKit.Data.Sqlite;
@@ -22,35 +23,40 @@ public class TestSQLiteKit : MonoBehaviour {
 	}
 
 	void Start () {
-		_dbFileName = Application.persistentDataPath + "/database.sqlite";
-		if (!File.Exists(_dbFileName)) {
-			SqliteConnection.CreateFile(_dbFileName);
-		}
-		using (var db = new SqliteConnection("URI=file:" + _dbFileName)) {
-			db.Open();
-			using (var cmd = db.CreateCommand()) {
-				cmd.CommandText = "DROP TABLE IF EXISTS Test; CREATE TABLE Test (val);";
-				var cnt = cmd.ExecuteNonQuery();
-				_guiText.text += "Created Table = " + cnt + "\n";
+		try {
+			_dbFileName = Application.persistentDataPath + "/database.sqlite";
+			_guiText.text += "db=" + _dbFileName + "\n";
+			if (!File.Exists(_dbFileName)) {
+				SqliteConnection.CreateFile(_dbFileName);
 			}
-			using (var cmd = db.CreateCommand()) {
-				InsertVal(cmd, 345);
-				InsertVal(cmd, "Test");
-			}
-			using (var cmd = db.CreateCommand()) {
-				cmd.CommandText = "SELECT val FROM Test;";
-				var val = cmd.ExecuteScalar();
-				_guiText.text += "Selected val = " + val + "\n";
-			}
-			using (var cmd = db.CreateCommand()) {
-				cmd.CommandText = "SELECT val FROM Test;";
-				using (var reader = cmd.ExecuteReader()) {
-					while (reader.Read()) {
-						var val = reader.GetValue(0);
-						_guiText.text += "Selected from Reader val = " + val + "\n";
+			using (var db = new SqliteConnection("URI=file:" + _dbFileName)) {
+				db.Open();
+				using (var cmd = db.CreateCommand()) {
+					cmd.CommandText = "DROP TABLE IF EXISTS Test; CREATE TABLE Test (val);";
+					var cnt = cmd.ExecuteNonQuery();
+					_guiText.text += "Created Table = " + cnt + "\n";
+				}
+				using (var cmd = db.CreateCommand()) {
+					InsertVal(cmd, 345);
+					InsertVal(cmd, "Test");
+				}
+				using (var cmd = db.CreateCommand()) {
+					cmd.CommandText = "SELECT val FROM Test;";
+					var val = cmd.ExecuteScalar();
+					_guiText.text += "Selected val = " + val + "\n";
+				}
+				using (var cmd = db.CreateCommand()) {
+					cmd.CommandText = "SELECT val FROM Test;";
+					using (var reader = cmd.ExecuteReader()) {
+						while (reader.Read()) {
+							var val = reader.GetValue(0);
+							_guiText.text += "Selected from Reader val = " + val + "\n";
+						}
 					}
 				}
 			}
+		} catch (Exception ex) {
+			_guiText.text += "exception=" + ex + "\n";
 		}
 	}
 }
